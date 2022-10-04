43cdd6a72423f02291ae43d95281136c31d5587d
Lennart Krebs <lennartkrebs@Lennarts-MBP.home>
2022-10-04 16:46:49 +0200

    Added basic structure

---
+++ scripts/brew.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+# Test for homebrew installation
+if test ! $(which brew)
+then
+  echo "Installing homebrew .."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Which apps to install
+apps=(
+    coreutils
+    tree
+    git
+    python3
+    tmux
+    neovim
+    tldr
+    jq
+    cmake
+    ffmpeg
+    wget
+)
+
+brew update 
+brew upgrade
+brew install "${apps[@]}"
+
