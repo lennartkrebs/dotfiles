590255427d0f21fa9e21a326cb4147cfd1b4d36d
Lennart Krebs <114749314+lennartkrebs@users.noreply.github.com>
2022-10-10 13:53:13 +0200

    Fixed brew install again

--- scripts/brew-install.zsh
+++ scripts/brew-install.zsh
@@ -7,10 +7,13 @@ sudo -v
 if ! which -s brew > /dev/null
 then
     echo "Homebrew is not installed. Installing.."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "# Set PATH, MANPATH, etc., for Homebrew." >> $HOME/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # Update, upgrade and install Brewfile
-brew update;
-brew upgrade;
-brew bundle install --no-lock;
+brew update
+brew upgrade
+brew bundle install --no-lock
