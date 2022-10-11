309068672510077162ee5e664eca6fa3dbc4dd8a
Lennart Krebs <114749314+lennartkrebs@users.noreply.github.com>
2022-10-11 10:38:46 +0200

    Commented out name settings

--- scripts/osx-defaults.zsh
+++ scripts/osx-defaults.zsh
@@ -13,9 +13,9 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set names
-sudo scutil --set ComputerName "Lennarts-MBP"
-sudo scutil --set HostName "Lennarts-MBP"
-sudo scutil --set LocalHostName "Lennarts-MBP"
+# sudo scutil --set ComputerName "Lennarts-MBP"
+# sudo scutil --set HostName "Lennarts-MBP"
+# sudo scutil --set LocalHostName "Lennarts-MBP"
 
 # Disable startup sound
 sudo nvram SystemAudioVolume=" "
